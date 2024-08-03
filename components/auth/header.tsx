f60c9dfb5bd00a88b733e18f4c9e3f7ad15d0f85
ravi kashyap <raviikashyap3@gmail.com>
2024-08-04 04:34:10 +0530

    create login form

--- components/auth/header.tsx
+++ components/auth/header.tsx
@@ -1,5 +1,6 @@
 import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Children } from "react";
 
 const font = Poppins({
     subsets:["latin"],
@@ -14,14 +15,17 @@ export const Header=({
     label
 }:HeaderProps) =>{
     return(
-        <div className="w-full flex flex-col gap-y-4 items-center">
+        <div className="w-full flex flex-col gap-y-4 items-center justify-center">
             <h1 className={cn(
                 "text-3xl font-semibold",
                 font.className,
             )}>
             🔐Auth
             </h1>
-
+            <p className="text-muted-foreground text-sm">
+                {label}
+            </p>
+            
         </div>
     )
 }
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,15 +1,63 @@
-import { CardWrapper } from "./card-wrapper"
+"use client";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginSchema } from "@/schemas";
 
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { CardWrapper } from "./card-wrapper";
+import { Resolver } from "dns";
+import { resolve } from "path";
+import { Input } from "@/components/ui/input";
 
-export const LoginForm =()=>{
+export const LoginForm = () => {
+    const form = useForm<z.infer<typeof LoginSchema>>({
+        resolver: zodResolver(LoginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
     return (
         <CardWrapper
-        headerLabel="Welcome Back"
-        backButtonLabel="don't have an account?"
-        backButtonHref="/auth/register"
-        showSocial
+            headerLabel="Welcome Back"
+            backButtonLabel="don't have an account?"
+            backButtonHref="/auth/register"
+            showSocial
         >
-            Login Forms
+            <Form {...form}>
+                <form
+                    className="space-y-6"
+                    onSubmit={form.handleSubmit(() => { })}
+                >
+                    <div className="space-y-4">
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Email</FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            {...field}
+                                            placeholder="abc@example.com"
+                                            type="email"
+                                        />
+                                    </FormControl>
+                                    <FormMessage/>
+                                </FormItem>
+    )}
+                        />
+                    </div>
+                </form>
+            </Form>
         </CardWrapper>
-    )
-}
+    );
+};
